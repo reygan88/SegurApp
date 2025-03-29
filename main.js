7d4099f67536c905177d70fecb38c9ecaf821aad
reygan88 <zelednix@gmail.com>
2025-03-29 11:29:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+// Esto es solo representativo. La lógica real está en React.
+// Para usarlo como app real, debe compilarse con React/Vite/Next.
+console.log("SegurApp está funcionando (versión demostración)");
